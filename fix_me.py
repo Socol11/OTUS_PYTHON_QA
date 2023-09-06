16938f961bf4a9a9a70d30eff4ca0383c5506bff
Roman <dumgym@gmail.com>
2023-09-06 18:27:47 +0300

    reset first files version

--- fix_me.py
+++ fix_me.py
@@ -1,14 +1,8 @@
-"""This module calculates averages"""
-
-
 def calculate_average(nums):
-    """Calculates average"""
     total = sum(nums)
-    count = len(nums)
+    count = len(numbers)
     average = total / count
-    return average
 
-
-numbers = [10, 15, 20]
-result = calculate_average(numbers)
-print("The average is:", result)
+nums = [10, 15, 20]
+result = calculat_average(nums)
+Print("The average is:", result)
